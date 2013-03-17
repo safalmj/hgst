b670e103cf070bd79f7ce6416ea8ff1120f2306d
safalmj <safalmj@gmail.com>
2013-03-17 16:33:05 +0545

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HGST::Application.config.secret_token = '3d0daefc717863732200ed8d60bcac4dc0b0c424b4920dbe7ea95ec605f59f3412d7ab6bcacbab1c00cb076e81d2733595254c68b67107215a8d69f9db4fd645'
